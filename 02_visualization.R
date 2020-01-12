7cc904eee1cfc99d99e6a8e584f901fac2bca724
lwawrowski <lukasz8989@gmail.com>
2020-01-12 13:35:10 +0100

    improve vizualizations

--- 02_visualization.R
+++ 02_visualization.R
@@ -1,5 +1,16 @@
 library(tidyverse)
 
+# https://www.r-bloggers.com/round-values-while-preserve-their-rounded-sum-in-r/
+
+round_preserve_sum <- function(x, digits = 0) {
+  up <- 10 ^ digits
+  x <- x * up
+  y <- floor(x)
+  indices <- tail(order(x-y), round(sum(x)) - sum(y))
+  y[indices] <- y[indices] + 1
+  y / up
+}
+
 load("data/hp_houses.RData")
 
 hp_houses <- hp_houses %>% 
@@ -25,13 +36,14 @@ hp_houses %>%
     theme(legend.position = "top", 
           plot.caption = element_text(color = "grey80")) +
     guides(fill = guide_legend(reverse = TRUE)) + 
+    ggtitle("Occurence of house name in Harry Potter books") +
     ggsave(filename = "figs/num_houses.png", width = 8, height = 6, dpi = 600)
 
 hp_houses %>% 
   group_by(book) %>%
   count(house) %>% 
   mutate(percent=n/sum(n),
-         percent_label=round(percent*100)) %>% 
+         percent_label=round_preserve_sum(percent*100)) %>% 
   ungroup() %>% 
   mutate(book_fct=factor(book, levels = unique(book), ordered = T),
          house_fct=factor(house, levels = unique(house), ordered = T)) %>% 
@@ -39,7 +51,7 @@ hp_houses %>%
   geom_col() + 
   geom_text(aes(label = percent_label), position = position_stack(vjust = 0.5), color = "white") +
   xlab("") +
-  ylab("Count") +
+  ylab("Percentage") +
   scale_fill_manual(values = rev(c("#670001", "#FF9D0A", "#002E5F", "#2E751C")), name = "") +
   scale_y_continuous(labels=scales::percent) +
   coord_flip() +
@@ -48,6 +60,7 @@ hp_houses %>%
   theme(legend.position = "top", 
         plot.caption = element_text(color = "grey80")) +
   guides(fill = guide_legend(reverse = TRUE)) + 
+  ggtitle("Occurence of house name in Harry Potter books (in %)") +
   ggsave(filename = "figs/num_houses_percent.png", width = 8, height = 6, dpi = 600)
 
 # number of pages in books
@@ -60,11 +73,19 @@ pages <- hp_houses %>%
 
 hp_houses %>% 
   count(book) %>% 
-  inner_join(., pages) %>% 
-  mutate(house_per_page=n/pages,
+  inner_join(., pages, by = "book") %>% 
+  mutate(house_per_page=round(n/pages,2),
          book_fct=factor(book, levels = unique(book), ordered = T)) %>% 
   ggplot(aes(x = fct_rev(book_fct), y = house_per_page)) +
-    geom_col() +
-    coord_flip()
+    geom_col(fill = "#FFC001") +
+    geom_text(aes(label = house_per_page), hjust = -0.2) +
+    coord_flip() +
+    xlab("") +
+    ylab("House per page") +
+    ylim(0,1.05) +
+    labs(caption = "Łukasz Wawrowski - wawrowski.edu.pl") +
+    theme_bw() +
+    ggtitle("House name mentioned per page in Harry Potter books") +
+    ggsave(filename = "figs/houses_page_percent.png", width = 8, height = 6, dpi = 600)
 
   
